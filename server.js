2c278a44ae6d585dcbd1c8b445c78aaba82eb22c
Johan <klausenjohan23@gmail.com>
2021-10-05 12:12:25 +0200

    used app.get and made some folders in the code

--- server.js
+++ server.js
@@ -6,30 +6,17 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8082;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    fs.readFile('Johan.html', (err, data) => {
+    fs.readFile('public/Johan.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Content-Length', data.length);
+       res.setHeader('Content-lengt', data.length)
         res.send(data);
 
     })
 })
-app.get('/Johan.css', (req, res) => {
-    fs.readFile('Johan.css', (err, data) => {
-        res.setHeader('Content-Type', 'text/css');
-        res.setHeader('Content-Length', data.length);
-        res.send(data);
-    })
 
-})
-app.get('/selfie.jpg', (req, res) => {
-    fs.readFile('selfie.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-       
-        res.send(data);
-    })
-
-})
 
 
 
